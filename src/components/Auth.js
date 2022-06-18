7f92589886d03cf758a9156b0627750bf4c9bb29
Shuvo <shuvocasanova@gmail.com>
2022-06-18 21:47:53 +0600

    Complete Cart

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth-slice";
 
 import "./Auth.css";
 
 const Auth = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Dispatch actions onSubmit
+    dispatch(authActions.login())
+  };
   return (
     <div className="container">
       <h1>Login</h1>{" "}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="id">Id</label>
         <input type="text" name="id" id="id" />
         <label htmlFor="password">Password</label>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../store/cart-slice";
 
 import "./Product.css";
 const Product = ({ name, id, imgURL, price }) => {
+  
+  const dispatch = useDispatch();
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    // Dispatch the click event
+    dispatch(cartActions.addToCart({ name, id, price }));
+  };
   return (
     <div className="card">
       <img src={imgURL} alt={name} />
       <h2>{name}</h2>
       <p>$ {price}</p>
-      <button>Add to cart</button>
+      <button onClick={handleClick}>Add to cart</button>
     </div>
   );
 };
